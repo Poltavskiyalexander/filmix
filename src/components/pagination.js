77175c1cbb01a6040d8b4940a86135b29600397d
Georgiy-Biryukov <zhorman0410@gmail.com>
2020-12-03 23:14:18 +0300

    commit pagination

---
+++ src/components/pagination.js
@@ -0,0 +1,136 @@
+import { parse } from 'handlebars';
+import templateSectionPagination from '../templates/section__pagination.hbs';
+
+//{ page, total_pages: totalPages }
+const pagegen = () => {
+  const obj = {
+    page: 1,
+    total_pages: 70,
+  };
+  const { page, total_pages: totalPages } = obj;
+
+  //-------твой код ------
+  const buffElem = document.createElement('div');
+
+  buffElem.insertAdjacentHTML('beforeend', templateSectionPagination());
+
+  let lists = buffElem.querySelector('.numpage__lists');
+
+  let parentList = buffElem.querySelector('.numpage');
+  let addlist = document.createElement('li');
+  let rightArrow = buffElem.querySelector('.numpage__right-arrow');
+  let leftArrow = buffElem.querySelector('.numpage__left-arrow');
+
+  const addPages = function () {
+    if (totalPages <= 1) {
+      parentList.remove();
+    }
+
+    const max = 9;
+
+    if (totalPages > 1) {
+      lists.insertAdjacentHTML(
+        'beforeend',
+        `<li class="numpage__list"><a class="numpage__list-item" href="#">${1}</a></li>`,
+      );
+
+      for (let i = page - 3; i <= page + 3; i++) {
+        let str = `<li class="numpage__list"><a class="numpage__list-item" href="#">${i}</a></li>`;
+        if (i <= 1 || i >= totalPages) {
+        } else {
+          if (i === page - 3) {
+            lists.insertAdjacentHTML(
+              'beforeend',
+              ` <li class="numpage__list numpage__more"><a class="numpage__list-item" href="#">...</a></li>`,
+            );
+          } else if (i === page + 3) {
+            lists.insertAdjacentHTML(
+              'beforeend',
+              ` <li class="numpage__list numpage__more"><a class="numpage__list-item" href="#">...</a></li>`,
+            );
+          } else {
+            lists.insertAdjacentHTML('beforeend', str);
+          }
+        }
+      }
+
+      lists.insertAdjacentHTML(
+        'beforeend',
+        `<li class="numpage__list"><a class="numpage__list-item" href="#">${totalPages}</a></li>`,
+      );
+      const itemsRefs = buffElem.querySelectorAll('.numpage__list-item');
+
+      itemsRefs.forEach(element => {
+        element.textContent == page
+          ? element.classList.add('item__border-active')
+          : null;
+      });
+    }
+    if (page === totalPages) {
+      rightArrow.remove();
+    }
+    if (page === 1) {
+      leftArrow.remove();
+    }
+
+    // Если количество в totalPAge меньше 9 то мы перебираем и создаём столько li, сколько указанно в totalPage
+
+    // if (totalPages < max) {
+    //   for (let i = 0; i < totalPages; i++) {
+    //     let str = `<li class="numpage__list"><a class="numpage__list-item" href="#">${
+    //       i + 1
+    //     }</a></li>`;
+    //     console.log(str);
+    //     lists.insertAdjacentHTML('beforeend', str);
+    //   }
+
+    //   // В противном случае будем создавать 9 штук
+    // } else {
+    //   for (let i = 0; i < max; i++) {
+    //     let str = `<li class="numpage__list"><a class="numpage__list-item" href="#">${
+    //       i + 1
+    //     }</a></li>`;
+    //     lists.insertAdjacentHTML('beforeend', str);
+    //   }
+    // }
+    //  Если количество в totalPAge больше чем 9(мах), то нужно перебрать и добавить на 1 элемент в текст-контент 1 ,
+    //  затем "...", потом 5 элементов, также "...", и номер totalPage.
+
+    //if(page < 3)
+    /*2 
+для 1 и 2 цикл до 7 добавляеш в конец ... 
+1) <- "1",2,3.4,5,6,7,...,500 ->, <- "1",2,3.4,5,6,7,...,500 ->
+2) <- "1",..,.4,5,6,7,...,500 ->, <- "1",2,3.4,5,6,7,...,500 ->
+если 
+
+*/
+
+    // for (let i = 0; i < 9; i++) {
+    // let addlist = document.createElement('li');
+    // lists.appendChild(addlist);
+    //   let str = `<li class="numpage__list"><a class="numpage__list-item" href="#">${
+    //     i + 1
+    //   }</a></li>`;
+    //   lists.insertAdjacentHTML('beforeend', str);
+    // }
+  };
+  const addListener = function () {
+    document.addEventListener('click', function (event) {
+      event.preventDefault();
+      console.log(event.target);
+    });
+  };
+
+  window.addEventListener('load', function () {
+    document
+      .getElementsByClassName('numpage__lists')
+      .addEventListener('click', addListener);
+  });
+
+  addPages();
+
+  //-------твой код конец ------
+  return buffElem.innerHTML;
+};
+
+export default pagegen;
--- src/pages/home.js
+++ src/pages/home.js
@@ -2,19 +2,16 @@ import templateHeader from '../templates/header.hbs';
 import templateSectionCards from '../templates/section__cards.hbs';
 import templateSectionPagination from '../templates/section__pagination.hbs';
 import templateFooter from '../templates/hooter.hbs';
+import pag from '../components/pagination';
 
 import medb from '../lib/ApiMEDB';
 
 const init = async () => {
-  medb.getPopularFilms().then(data => {
-    const { id, poster_path } = data;
-    console.log(data);
-  });
-
   const duffElem = document.createElement('div');
   duffElem.insertAdjacentHTML('beforeend', templateHeader());
   duffElem.insertAdjacentHTML('beforeend', templateSectionCards());
-  duffElem.insertAdjacentHTML('beforeend', templateSectionPagination());
+  //duffElem.insertAdjacentHTML('beforeend', templateSectionPagination());
+  duffElem.insertAdjacentHTML('beforeend', pag());
   duffElem.insertAdjacentHTML('beforeend', templateFooter());
   return duffElem.innerHTML;
 };
