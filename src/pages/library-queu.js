0f2ff02f8bce6cda5db8a69b66e6b1031a7c63db
Tatyana <tannyhappiness@gmail.com>
2020-12-04 13:37:36 +0200

    ls

--- src/pages/library-queu.js
+++ src/pages/library-queu.js
@@ -20,7 +20,33 @@ export default init;
 const submitWatched = event => {
   const id = event.currentTarget.dataset.name;
   localStorage.setItem('Watched ID', id);
+
+  const refs = {
+    buttonWatched: document.querySelector('action__watched'),
+    buttonQueue: document.querySelector('action_queue'),
+  };
+
+  refs.buttonWatched.classList.add('active');
+  // let getFilmWatchedId = localStorage.getItem('Watched ID');
+
+  // if (getFilmWatchedId) {
+  // } else
 };
+
+// ПРИМЕР
+//   if (!getItemKeyTheme) {
+//     refs.body.classList.add(Theme.LIGHT);
+//     getItemKeyTheme = Theme.LIGHT;
+//   } else {
+//     refs.body.classList.add(getItemKeyTheme);
+//   }
+
+//   if (getItemKeyTheme === Theme.DARK) {
+//     refs.switchInput.checked = true;
+//   } else {
+//     getItemKeyTheme = false;
+//   }
+// };
 
 const submitQueue = event => {
   const id = event.currentTarget.dataset.name;
