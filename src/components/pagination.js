4f548c2327de7ce53804e4a9fc05929981b148dd
Anastasiya Teslenko <tislenko633@gmail.com>
2020-12-11 00:52:36 +0200

    test2Joraimg

--- src/components/pagination.js
+++ src/components/pagination.js
@@ -19,12 +19,66 @@ const paginationInit = (obj, url) => {
       parentList.remove();
     }
     if (totalPages > 1) {
-      for (let i = page - 2; i <= page + 2 && i <= totalPages; i++) {
-        if (i >= 1) {
-          let str = paginationList({ page: i, url, more: true });
+      if (false) {
+        for (let i = page - 2; i <= page + 2 && i <= totalPages; i++) {
+          if (i >= 1) {
+            let str = paginationList({ page: i, url, more: true });
 
-          lists.insertAdjacentHTML('beforeend', str);
+            lists.insertAdjacentHTML('beforeend', str);
+          }
         }
+      } else {
+        lists.insertAdjacentHTML(
+          'beforeend',
+          paginationList({ page: 1, url, more: true, class: 'media-hidden' }),
+        );
+
+        for (let i = page - 3; i <= page + 3; i++) {
+          let str = paginationList({ page: i, url, more: true });
+          if (i <= 1 || i >= totalPages) {
+          } else {
+            if (i === page - 3) {
+              lists.insertAdjacentHTML(
+                'beforeend',
+                paginationList({
+                  class: 'numpage__more media-hidden',
+                  page: '...',
+                  url,
+                  more: false,
+                }),
+              );
+            } else if (i === page + 3) {
+              lists.insertAdjacentHTML(
+                'beforeend',
+                paginationList({
+                  class: 'numpage__more  media-hidden',
+                  page: '...',
+                  url,
+                  more: false,
+                }),
+              );
+            } else {
+              lists.insertAdjacentHTML('beforeend', str);
+            }
+          }
+        }
+
+        lists.insertAdjacentHTML(
+          'beforeend',
+          paginationList({
+            page: totalPages,
+            url,
+            more: true,
+            class: 'media-hidden',
+          }),
+        );
+        const itemsRefs = buffElem.querySelectorAll('.numpage__list-item');
+
+        itemsRefs.forEach(element => {
+          element.textContent == page
+            ? element.classList.add('item__border-active')
+            : null;
+        });
       }
 
       const itemsRefs = buffElem.querySelectorAll('.numpage__list-item');
@@ -54,57 +108,6 @@ const paginationInit = (obj, url) => {
 
     const max = 9;
     if (totalPages > 1) {
-      lists.insertAdjacentHTML(
-        'beforeend',
-        paginationList({ page: 1, url, more: true, class: 'media-hidden' }),
-      );
-
-      for (let i = page - 3; i <= page + 3; i++) {
-        let str = paginationList({ page: i, url, more: true });
-        if (i <= 1 || i >= totalPages) {
-        } else {
-          if (i === page - 3) {
-            lists.insertAdjacentHTML(
-              'beforeend',
-              paginationList({
-                class: 'numpage__more media-hidden',
-                page: '...',
-                url,
-                more: false,
-              }),
-            );
-          } else if (i === page + 3) {
-            lists.insertAdjacentHTML(
-              'beforeend',
-              paginationList({
-                class: 'numpage__more  media-hidden',
-                page: '...',
-                url,
-                more: false,
-              }),
-            );
-          } else {
-            lists.insertAdjacentHTML('beforeend', str);
-          }
-        }
-      }
-
-      lists.insertAdjacentHTML(
-        'beforeend',
-        paginationList({
-          page: totalPages,
-          url,
-          more: true,
-          class: 'media-hidden',
-        }),
-      );
-      const itemsRefs = buffElem.querySelectorAll('.numpage__list-item');
-
-      itemsRefs.forEach(element => {
-        element.textContent == page
-          ? element.classList.add('item__border-active')
-          : null;
-      });
     }
     if (page === totalPages) {
       rightArrow.remove();
