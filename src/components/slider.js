d08a7effa155f2ba87db2d7f9cb0d04b5f8a1ac9
Georgiy-Biryukov <zhorman0410@gmail.com>
2020-12-11 01:02:38 +0300

    slider

--- src/components/slider.js
+++ src/components/slider.js
@@ -4,6 +4,13 @@ import templateSlider from '../templates/section_slider.hbs';
 const init = data => {
   const duffElem = document.createElement('div');
   duffElem.insertAdjacentHTML('beforeend', templateSlider(data));
+
+  duffElem.getElementsByClassName('left').onclick = slideClick;
+  function slideClick() {
+    const polosa = duffElem.getElementsByClassName('slider__lists');
+    polosa.style.left = -800 + 'px';
+  }
+
   return duffElem.innerHTML;
 };
 
