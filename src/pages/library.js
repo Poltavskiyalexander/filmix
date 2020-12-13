acf2d5355f0732f3499772b29133df440fc143ac
PoltavskiyAlexander <Poltavskiyalexander@gmail.com>
2020-12-13 14:42:40 +0200

    fix lib

--- src/pages/library.js
+++ src/pages/library.js
@@ -40,12 +40,20 @@ const init = async (params, query) => {
 
   // let key = KEY_QUEUE;
   const ls = new storage();
-  const data = await medb.getFilmsArrId(ls.get(key), qweryObj.page);
-  const { genres: genresArr } = await medb.getGenresList(data);
-  const results = Build(data, genresArr);
+  let data;
+  if (ls.has(key)) {
+    data = await medb.getFilmsArrId(ls.get(key), qweryObj.page);
+    const { genres: genresArr } = await medb.getGenresList(data);
+    const results = Build(data, genresArr);
+  }
 
   const duffElem = document.createElement('div');
-  duffElem.insertAdjacentHTML('beforeend', baseMarkup(data, url));
+  if (ls.has(key)) {
+    duffElem.insertAdjacentHTML('beforeend', baseMarkup(data, url));
+  } else {
+    duffElem.insertAdjacentHTML('beforeend', baseMarkup());
+  }
+
   duffElem.querySelector('.form-search-library').remove();
   duffElem.querySelector('header').classList.add('header__img-watched');
 
